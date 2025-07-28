edf0e5f9cd8cb75f334c7ebdb71697a3936e7014
josemanuelRS <pin2_x@hotmail.com>
2025-07-28 19:49:44 +0200

    task 2

--- database/seeders/GolferSeeder.php
+++ database/seeders/GolferSeeder.php
@@ -9,9 +9,12 @@ class GolferSeeder extends Seeder
 {
     public function run(): void
     {
+        // Get the maximum existing debitor_account, or 0 if there is none
+        $lastDebitorAccount = Golfer::max('debitor_account') ?? 0;
+
         for ($i = 1; $i <= 100; $i++) {
             Golfer::factory()->create([
-                'debitor_account' => $i,
+                'debitor_account' => $lastDebitorAccount + $i,
             ]);
         }
     }
